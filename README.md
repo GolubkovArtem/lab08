0837185e892577009c56993241970a873ec46a25
GolubkovArtem <artemgolubkov@bk.ru>
2017-10-22 17:16:20 +0300

    added doxygen.conf

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-[![Build Status](https://travis-ci.org/GolubkovArtem/lab06.svg?branch=master)](https://travis-ci.org/GolubkovArtem/lab06)
+[![Build Status](https://travis-ci.org/GolubkovArtem/lab07.svg?branch=master)](https://travis-ci.org/GolubkovArtem/lab07)
  
--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,15 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-
+//!string output function to output stream
+/*!
+      \param text an argument of type std::string
+      \param out an argument of type std::ostream, the default is std::cout
+     */
 void print(const std::string& text, std::ostream& out = std::cout);
+//!string output function to a file stream
+/*!
+      \param text an argument of type std::string
+      \param out an argument of type std::ofstream
+     */
 void print(const std::string& text, std::ofstream& out);
